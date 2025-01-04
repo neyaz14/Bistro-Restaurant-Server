5faca5e714f591b994d0808458eeecb8c5545fa0
Neyaz Morshid <neyazmorshid@gmail.com>
2025-01-04 10:01:10 +0600

    basic setup done

--- index.js
+++ index.js
@@ -25,6 +25,16 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
+    const menuCollection = client.db("BistroBoss").collection("menu");
+    const reviewCollection = client.db("BistroBoss").collection("reviews");
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+  });
+  app.get('/reviews', async (req, res) => {
+    const result = await reviewCollection.find().toArray();
+    res.send(result);
+});
 
 
     // Send a ping to confirm a successful connection
